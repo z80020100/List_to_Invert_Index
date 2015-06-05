5bdfe2c4952e1680272224c3f1fd720a8b71f915
z80020100 <z800201002005@gmail.com>
2015-06-06 03:46:25 +0800

    Finish

--- List_to_Invert_Index/main.cpp
+++ List_to_Invert_Index/main.cpp
@@ -22,33 +22,40 @@ int main()
 	LARGE_INTEGER startTime, endTime, fre;
 	double times;
 
-	QueryPerformanceFrequency(&fre); // 取得CPU頻率
-	QueryPerformanceCounter(&startTime); // 取得開機到現在經過幾個CPU Cycle
-	/* Program to Timing */
-	DIR *dp;
-	struct dirent *ep;
-	string list_path = "./List_Multi_File/";
+	string list_path = "./List/Forward_index.list";
 	
 	fstream list_file, index_file, log_file;
-	string file_name, index_path, log_path = "./List_to_Invert_index_Log.txt";
+	string index_path, log_path = "./List_to_Invert_index_Log.txt";
 
-	char buf[1024];
-	string keyword, max_keyword, max_file_ID, max_number_keyword;
+	//char buf[1024];
+	string buf;
+	string keyword, max_keyword, max_number_keyword, temp_ID;
 	int max_length = 0, temp_length, keyword_number, temp_number, max_file_number = 0;
 
 	map < string, vector<int> > invert_index_map;
 	map < string, vector<int> >::iterator it;
-	int file_ID;
+	int file_ID, max_file_ID;
 
+	int start, end;
+
+	QueryPerformanceFrequency(&fre); // 取得CPU頻率
+	QueryPerformanceCounter(&startTime); // 取得開機到現在經過幾個CPU Cycle
+	/* Program to Timing */
+	cout << "Create log file: " << log_path << endl;
 	log_file.open(log_path, ios::out);
 	if (!log_file)
 		cerr << "Error: can not create log file!" << endl;
-
-	dp = opendir(list_path.c_str()); // for each file f, create a list f_bar of unique keyword
-	if (dp != NULL)
+	cout << "Open source forward index file: " << list_path << endl;
+	list_file.open(list_path, ios::in);
+	if (!list_file)
+	{
+		cerr << "Error: open forward index: " << list_path << " failed..." << endl;
+		log_file << "Error: open forward index: " << list_path << " failed..." << endl;
+	}
+	else
 	{
 		index_path = "./Index/Invert_Index.idx";
-		//cout << index_path << endl;
+		cout << "Target index file: " << index_path << endl;
 		index_file.open(index_path, ios::out | ios::binary);
 		if (!index_file)
 		{
@@ -57,49 +64,53 @@ int main()
 		}
 		else
 		{
-			readdir(dp); // .
-			readdir(dp); // ..
-			while (ep = readdir(dp)) // read the list file, the list file need to be UNIX format
+			while (getline(list_file, buf))
 			{
-				//printf("%s\n", ep->d_name);
-				file_name.assign(ep->d_name);
-
-				list_path = "./List/";
-				list_path.append(file_name);
-				list_file.open(list_path, ios::in);
-				if (!list_file)
-					cerr << "Error: open: " << list_path << " failed..." << endl;
-				else
+				keyword_number = 0;
+				/* Find the file ID */
+				for (int i = 0; i < buf.size(); i++)
 				{
-					memset(buf, 0, sizeof(buf));
-					keyword_number = 0;
-					file_name.erase(file_name.end() - 5, file_name.end());
-					file_ID = atoi(file_name.c_str());
-					//cout << file_ID << endl;
-					while (list_file.getline(buf, sizeof(buf)))
+					if (buf[i] == ':')
 					{
+						temp_ID.assign(buf.c_str(), i); // i: the length of the keyword
+						file_ID = atoi(temp_ID.c_str());
+						//cout << "File ID: " << file_ID << endl;
+						start = i + 1; // set the begining index of the file ID
+						break;
+					}
+				}
+				/* Find the file ID */
+
+				/* Find the keyword */
+				for (int i = start; i < buf.size(); i++)
+				{
+					if (buf[i] == 32) // 32 is space in ASCII
+					{
+						end = i;
+						keyword.assign(&buf[start], end - start);
 						keyword_number++;
-						keyword.assign(buf);
-						//cout << keyword << endl;
 						temp_length = keyword.size();
 						if (temp_length > max_length)
 						{
 							max_length = keyword.size();
 							max_keyword = keyword;
-							max_file_ID = file_name;
+							max_file_ID = file_ID;
 						}
-						memset(buf, 0, sizeof(buf));
+						//cout << keyword << endl;
+						start = end + 1;
 						invert_index_map[keyword].push_back(file_ID);
-
+						invert_index_map[keyword].shrink_to_fit();
+						//cout << keyword << " " << file_ID << endl; // show the pairs
 					}
-					//cout << "          File ID: " << file_name << endl; 
-					//cout << "Number of keyword: " << keyword_number << endl << endl;
-					log_file << "          File ID: " << file_name << ", ";
-					log_file << "Number of keyword: " << keyword_number << endl << endl;
-
-					list_file.close();
 				}
+
+				//cout << "          File ID: " << file_ID << endl; 
+				//cout << "Number of keyword: " << keyword_number << endl << endl;
+				log_file << "          File ID: " << file_ID << ", ";
+				log_file << "Number of keyword: " << keyword_number << endl << endl;
+				/* Find the keyword */
 			}
+			list_file.close();
 
 			for (it = invert_index_map.begin(); it != invert_index_map.end(); it++)
 			{
@@ -118,20 +129,20 @@ int main()
 					index_file << invert_index_map[keyword][i] << " ";
 				}
 				//cout <<endl;
-				index_file <<endl;
+				index_file << endl;
 			}
-
 			index_file.close();
 		}
 	}
-	
+	/* Program to Timing */
+	QueryPerformanceCounter(&endTime); // 取得開機到程式執行完成經過幾個CPU Cycle
+	times = ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart;
+
+	cout << endl;
 	cout << "The maximum length of keyword: " << max_length << " in file ID: " << max_file_ID << endl;
 	cout << "The maximum keyowrd: " << max_keyword << endl;
 	log_file << "The maximum length of keyword: " << max_length << " in file ID: " << max_file_ID << endl;
 	log_file << "The maximum keyword: " << max_keyword << endl;
-	/* Program to Timing */
-	QueryPerformanceCounter(&endTime); // 取得開機到程式執行完成經過幾個CPU Cycle
-	times = ((double)endTime.QuadPart - (double)startTime.QuadPart) / fre.QuadPart;
 
 	cout << "The maximum number of file including a keyword: " << max_file_number << ", the keyword is: " << max_number_keyword << endl;
 	log_file << "The maximum number of file including a keyword: " << max_file_number << ", the keyword is: " << max_number_keyword << endl;
